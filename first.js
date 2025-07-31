4f2808a0d45e80c1ba33ebd6e976855739245ae2
armanh148 <armannaqvi.tms@gmail.com>
2025-07-31 16:31:15 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,34 @@
+// show hello world //
+alert("hello world");
+console.log("hello world");
+document.write("hello world");
+
+// create variable //
+let name = "arman";
+let age = 20;
+let isstudent = true;
+
+// show them using them console //
+console.log("Name", name);
+console.log("age", age);
+console.log("isstudent", isstudent);
+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+// show result in console //
+console.log("Sum", sum);
+console.log("Different", difference);
+console.log("Product", product);
+console.log("Quotient", quotient);
+
+
+//show button function //
+function showMessage() {
+    alert("Button was clicked!");
+}
